63d745624b13f83feeab06dce6231bf6decfebff
Jackson Ming Hu <huming2207@gmail.com>
2018-07-23 09:11:40 +1000

    Test: init

---
+++ src/test/java/auth/SimpleAuthProviderTest.java
@@ -0,0 +1,34 @@
+package auth;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SimpleAuthProviderTest
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+    @Test
+    public void requestLoginPage()
+    {
+    }
+
+    @Test
+    public void performLogin()
+    {
+    }
+
+    @Test
+    public void validateLoginToken()
+    {
+    }
+}
---
+++ src/test/java/timetable/TimetableQueryTest.java
@@ -0,0 +1,34 @@
+package timetable;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TimetableQueryTest
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+    @Test
+    public void fetchAllocatedCourses()
+    {
+    }
+
+    @Test
+    public void fetchEnrolledCourses()
+    {
+    }
+
+    @Test
+    public void setActivity()
+    {
+    }
+}
