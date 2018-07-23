cfb44dc32216ec504b24e6bf7b5e43a805b93259
Jackson Ming Hu <huming2207@gmail.com>
2018-07-23 23:02:33 +1000

    PortalQuery: add query methods

--- src/main/java/helpers/Constant.java
+++ src/main/java/helpers/Constant.java
@@ -4,6 +4,7 @@ public class Constant
 {
     public static final String SSO_CAS_BASE_URL = "https://sso-cas.rmit.edu.au/rmitcas/login";
     public static final String TIMETABLE_BASE_URL = "https://mytimetable.rmit.edu.au/even/rest";
+    public static final String PORTAL_BASE_URL = "https://my.rmit.edu.au/service";
     public static final String SETTING_KEY_USER_AGENT = "okhttp_ua";
     public static final String SETTING_KEY_USERNAME = "username";
     public static final String SETTING_KEY_PASSWORD = "password";
--- src/main/java/portal/PortalQuery.java
+++ src/main/java/portal/PortalQuery.java
@@ -1,10 +1,20 @@
 package portal;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import helpers.Constant;
+import helpers.HttpFetcherSync;
 import okhttp3.CookieJar;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import portal.announcement.Announcement;
+import portal.detail.Detail;
+import portal.exam.Exam;
+import portal.result.Result;
 
+import java.io.IOException;
 import java.util.Properties;
 
 public class PortalQuery
@@ -20,5 +30,37 @@ public class PortalQuery
         this.httpClient = new OkHttpClient.Builder().cookieJar(cookieJar).build();
     }
 
+    private <T> T fetchPortalApis(String endpoint, TypeReference<T> resultType) throws IOException
+    {
+        String queryUrl = String.format("%s%s", Constant.PORTAL_BASE_URL, endpoint);
+        Request request = new Request.Builder()
+                .url(queryUrl)
+                .addHeader("User-Agent", prop.getProperty(Constant.SETTING_KEY_USER_AGENT))
+                .build();
 
+        String responseJson = HttpFetcherSync.performRequest(this.httpClient, request, this.logger);
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        return objectMapper.readValue(responseJson, resultType);
+    }
+
+    public Result fetchMyExamResult() throws IOException
+    {
+        return this.fetchPortalApis("/myexamresults", new TypeReference<Result>() {});
+    }
+
+    public Exam fetchMyExamTimetable() throws IOException
+    {
+        return this.fetchPortalApis("/myexamtimetable", new TypeReference<Exam>() {});
+    }
+
+    public Detail fetchMyDetail() throws IOException
+    {
+        return this.fetchPortalApis("/mydetails", new TypeReference<Detail>() {});
+    }
+
+    public Announcement fetchAnnouncement() throws IOException
+    {
+        return this.fetchPortalApis("/announcements", new TypeReference<Announcement>() {});
+    }
 }
